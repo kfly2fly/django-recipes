c64f7babc3befb5bb5610f0f59038f88029c04a1
Keenan Flynn <k.p.flynn70@gmail.com>
2024-12-20 13:07:27 -0700

    feat(deploy): docker deploy config

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_s7s83#p0bc0ji%+npmkd!3ft&w=m*jp(y=t&3a(ld%@v+2lv7"
+SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = []
 
