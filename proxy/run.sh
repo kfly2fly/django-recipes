a0c8d36b60d33689a76c21b7e0040690528544fa
Keenan Flynn <k.p.flynn70@gmail.com>
2024-12-23 12:04:12 -0700

    fix(deploy): https permission issues

--- proxy/run.sh
+++ proxy/run.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+# Ensure SSL directory ownership and permissions
+if [ -d /etc/nginx/ssl ]; then
+  echo "Setting permissions for /etc/nginx/ssl"
+  chown -R nginx:nginx /etc/nginx/ssl
+  chmod -R 750 /etc/nginx/ssl
+fi
+
 # Check for SSL certificates
 if [ ! -f /etc/nginx/ssl/certificate.crt ] || [ ! -f /etc/nginx/ssl/private.key ]; then
     echo "Error: SSL certificate files not found in /etc/nginx/ssl/"
